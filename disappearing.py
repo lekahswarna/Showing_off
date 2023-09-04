bde78de1ff4320cc59ee406054a54f36dc66e0fa
lekahswarna <141463968+lekahswarna@users.noreply.github.com>
2023-09-04 22:26:47 +0530

    disappearing.py

--- disappearing.py
+++ disappearing.py
@@ -1,22 +1,19 @@
 import tkinter as tk
-from tkinter import ttk
 
-def fade_text():
-    for alpha in range(255, 0, -10):
-        text_label.configure(foreground=f'#{alpha:02x}{alpha:02x}{alpha:02x}')
-        root.update()
-        root.after(100)  # Delay in milliseconds
+def clear_text():
+    text.delete(1.0, tk.END)
 
-root = tk.Tk()
-root.title("Disappearing Text")
+# Create the main window
+window = tk.Tk()
+window.title("Text Disappearing App")
 
-frame = ttk.Frame(root)
-frame.pack(padx=20, pady=20)
+# Create a Text widget for typing text
+text = tk.Text(window, height=10, width=40)
+text.pack()
 
-text_label = ttk.Label(frame, text="Text will disappear when you click the button.")
-text_label.pack(padx=10, pady=10)
+# Create a button to clear the text
+clear_button = tk.Button(window, text="Clear Text", command=clear_text)
+clear_button.pack()
 
-fade_button = ttk.Button(frame, text="Fade Text", command=fade_text)
-fade_button.pack(padx=10, pady=10)
-
-root.mainloop()
+# Start the main loop
+window.mainloop()
